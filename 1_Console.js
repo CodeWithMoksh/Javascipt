3757575b7370631f5e2cedad7abaf5cddc25e11a
Moksh <jmoksh846@gmail.com>
2025-05-26 20:49:20 +0530

    Updated each file extension to js

---
+++ 1_Console.js
@@ -0,0 +1,8 @@
+// From here we are going to start our JAVASCRIPT JOURNEY
+// About Console elements or console api
+    console.log("Hello World");
+    console.warn("This is warning"); //Will be going to show in the console as a warning in yellow color
+    console.error("It is a Favicon error"); //Will be going to show in the console as a error like a favicon error in red color
+    console.assert(6 == 7, "The provided numbers are not equal"); //Writes an error message to the console if the assertion is false
+    console.clear(); //Will be going to Clear the console
+    
